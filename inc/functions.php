e79af99d3bf42c9a132623faa3e9339297e41660
Brandon-Crosby <53662951+Brandon-Crosby@users.noreply.github.com>
2019-08-28 20:23:56 -0400

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,46 +1,49 @@
+
+
+<?php
+
+
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
 
-<?php
-
 $quotes = array(
 
 [
     'quote' =>  "Don't be sad it's over, be happy it happened "  ,
-    'source' => ' - Dr. Seuss'  ,
-    'year' => '',
+    'source' => 'Dr. Seuss'  ,
+    'year' => 1983  ,
     'tags'=>  'Inspirational'     ,
 ],
 [
       'quote' =>  'Be yourself, everyone is already taken'  ,
-      'source' =>  ' - Oscar Wilde' ,
-      'year' =>  '' ,
+      'source' =>  'Oscar Wilde' ,
+      'year' => 1988  ,
       'tags'=>   'Inspirational'    ,
 ],
 [
       'quote' =>  'Be the change that you wish to see in the world'  ,
-      'source' =>  ' - Gandhi' ,
-      'year' => ''  ,
+      'source' =>  'Gandhi' ,
+      'year' =>  1953 ,
       'tags'=>      'Inspirational' ,
 ],
 [
       'quote' =>  'It is never to late to be what you might have been'  ,
-      'source' => ' - George Eliot'  ,
-      'year' => ''  ,
+      'source' => 'George Eliot'  ,
+      'year' =>  1763 ,
       'tags'=>      'Inspirational' ,
 ],
 [
       'quote' =>   'Everything you can imagine is real' ,
       'source' =>   'Pablo Escobar',
-      'year' => ''  ,
+      'year' =>  1985,
       'tags'=>     'Inspirational'  ,
 ],
 [
       'quote' =>   'Talk is cheap. Show me the code' ,
       'source' =>   'Linus Torvalds',
-      'year' => '2000 '  ,
+      'year' => 2000  ,
       'tags'=>     'Inspirational'  ,
 ],
 );
@@ -71,40 +74,27 @@ global $quotes;
 $This_quote = getRandomQuote($quotes);
 
 $HTML_quote = '';
-$HTML_quote .= '<p class="quote">'. $This_quote['quote'] . '</p>';
+$HTML_quote .= '<p class="quote">'. $This_quote["quote"] . '</p>';
 $HTML_quote .= '<p class="source">' . $This_quote['source'] ;
 //'<span class="year">' . $This_quote['year'] . '</span>' .
 //'<span class="tags">' . $This_quote['tags'] . '</span> </p>';
 if ($This_quote['year']){
-$HTML_quote .= '<span class="year">' . $This_quote["year"] . '</span>';
+  $HTML_quote .= '<span class="year">' . $This_quote['year'] . '</span>';
 }
 if ($This_quote['tags']){
-$HTML_quote .= '<span class="tags">' . $This_quote["tags"] . '</span>';
+  $HTML_quote .= '<br> <span class="tags">' . $This_quote["tags"] . '</span> </br>';
 }
-/*if ($This_quote['year']&&['tags'] == Null){
-$HTML_quote .= '<p class="quote">' . $This_quote['quote'] . '</p>';
-$HTML_quote .= '<p class="quote">' . $This_quote['source'] . '</p>' ;
-}*/
-$HTML_quote .= '</p>';
+$HTML_quote .= "</p>";
 echo $HTML_quote;
 }
 
+//background color change
+$colors= array("#AB1DAB","#B31A96","#BB1782","#C3146D","#CB1259","#D30F44","#DB0C30","#E3091B","#EB0707");
+$colorChange = $colors[array_rand($colors)];
+ return $colorChange ;
 
 
 
-
-
-
-//background color
-
-
-
+//refreshes page after 15 seconds -
+header("refresh: 15");
 ?>
-
-
-
-
-
-
-
-source
